46c5a22e7dfe73b748d939d7b4f779d00dd38c0c
rrahulraj2003 <rrahulraj2003@gmail.com>
2024-09-17 17:43:20 -0400

    first commit

---
+++ release/stack.c
@@ -0,0 +1,38 @@
+/*
+* Add NetID and names of all project partners
+*
+*/
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Part 1 - Sztep 1 and 2: Do your tricks here
+ * Your goal must be to change the stack frame of caller (main function)
+ * such that you get to the line after "r2 = *( (int *) 0 )"
+ */
+void signal_handle(int signalno) {
+
+    int i = 0; 	
+    printf("handling segmentation fault!\n");
+    i = i + 1; //just incrementing a variable.
+
+    /* Step 2: Handle segfault and change the stack*/
+
+}
+
+int main(int argc, char *argv[]) {
+
+    int r2 = 0;
+
+    /* Step 1: Register signal handler first*/
+
+    signal_handle(SIGSEGV);
+    r2 = *( (int *) 0 ); // This will generate segmentation fault
+    
+    int p = *((int *) 0x000055555555521d);
+
+    r2 = r2 + 1 * 45;
+    printf("result after handling seg fault %d!\n", r2);
+
+    return 0;
+}
